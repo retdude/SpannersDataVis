8b4a7f52d44463cd0a169b17a3a0597a9e288e59
Luke Spagnoli <spagnoli.luke@gmail.com>
2022-12-07 21:47:17 -0500

    Update to spanner function

--- spanner.py
+++ spanner.py
@@ -1,7 +1,7 @@
 import networkx as nx
 import csv
 import matplotlib.pyplot as plt #visualization
-import random as rand
+import numpy as np
 
 def main():
     #Main Function
@@ -10,7 +10,7 @@ def main():
     G = handleCSV()
     H = nx.create_empty_copy(G, with_data=True) #creates edgeless graph copy
 
-    #testDisplay(G, isFinished)
+    testDisplay(G, isFinished)
     #testDisplay(H, isFinished)
 
     mst = nx.minimum_spanning_tree(G, weight='weight', algorithm='kruskal', ignore_nan=False)
@@ -36,7 +36,7 @@ def setEdgeWeight(G):
     nx.set_edge_attributes(G, values = 1, name = 'weight')
 
     for u,v,d in G.edges(data=True):
-        d['weight']+= rand.randint(0,9)
+        d['weight']+= np.random.uniform(0, 10)
 
     
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -46,10 +46,13 @@ def testDisplay(G, isFinished):
     for u,v,d in G.edges(data=True):
         print(u,v,d)
 
-    print()
+    print("~~~~~~~~~~~~~~~~~")
 
-    print("Graph output:", G)
-    print("Size = ", G.size(weight="weight"))
+    print("Graph Output:", G)
+    print("Sum of Edge Weight = ", G.size(weight="weight"))
+
+    print("~~~~~~~~~~~~~~~~~")
+    print()
 
     pos = nx.spring_layout(G,scale=1)
     nx.draw(G, pos, with_labels=True)
@@ -81,18 +84,41 @@ def spanning(G, H):
     """  
     Pseudocode:
 
-    get mst of G
-    subdivide the edges
-    add all the edges of the mst into h(spanner)
+    get mst of G (DONE)
+    subdivide the edges (??)
+    add all the edges of the mst into h(spanner) (SORTA DONE)
     apply the d-light weight initialization sort the neightbor edges for each node accorting to the sorted order, add them to h and sum the edge weights for each node
     keep adding the edges until the total sum is = D
     spanner completion for each vertex try to add all edges in the shortest path if the spanner condition doesn't satisfy 
     """
 
     mst = nx.minimum_spanning_tree(G) #get mst of G
+    sum = mst.size(weight="weight")
+    n = mst.number_of_nodes()
 
+    for u,v,d in G.edges(data=True): #scaling
+        d['weight'] *= (n/2)*(1/sum)
+
+
+    for u,v,d in G.edges(data=True): #subdivision w(e) / ceiling(w(e))
+        #iterate through all the edges, if > 1: split each edge into mini edges and then fill those gaps with nodes
+        #then add those mini edges and nodes to H
+        #if <= 1, add anyway
+
+        print()
+
+    #d- light weight
+    #add mst to H, then add edges from g' until the sum <= d
+
+    #spanner completion
+    #take each pair of vertex, calc the shortest path from u,v in H and u,v in G' check how much larger is H
+    #if it is a lot larger and doesn't satisfy the equation, add all the edges into H from G' (for u,v)
+    #do until this has been done for all vertex pairs
+    #if there is a pair that is not in H but in G', then just add from G' to H
 
 
     return(H)
+
+
 
 main()
