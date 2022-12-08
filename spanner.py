a04a70a0e373d42b8094db9d977128e0694702ed
Luke Spagnoli <spagnoli.luke@gmail.com>
2022-12-08 11:56:32 -0500

    Completed Scale and subdivide

--- spanner.py
+++ spanner.py
@@ -2,6 +2,7 @@ import networkx as nx
 import csv
 import matplotlib.pyplot as plt #visualization
 import numpy as np
+import math
 
 def main():
     #Main Function
@@ -10,12 +11,18 @@ def main():
     G = handleCSV()
     H = nx.create_empty_copy(G, with_data=True) #creates edgeless graph copy
 
-    testDisplay(G, isFinished)
-    #testDisplay(H, isFinished)
+    testDisplay(G, isFinished) #gets starting graph image
 
-    mst = nx.minimum_spanning_tree(G, weight='weight', algorithm='kruskal', ignore_nan=False)
+    Gprime = scaleGraph(G) #creates G'
 
-    testDisplay(mst, isFinished)
+    subdivideGraph(Gprime, H)
+
+    testDisplay(Gprime, isFinished)
+    testDisplay(H, isFinished)
+    
+
+    print("DONE")
+   
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 def handleCSV():
@@ -36,8 +43,20 @@ def setEdgeWeight(G):
     nx.set_edge_attributes(G, values = 1, name = 'weight')
 
     for u,v,d in G.edges(data=True):
-        d['weight']+= np.random.uniform(0, 10)
+        d['weight']+= round(np.random.uniform(0, 50), 2)
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def nodeCreation(G):
+    #creates new node to be added
+
+    checkList = list(G.nodes())
+
+    largest = 0;
+    for node in checkList:
+        if int(node) > largest:
+            largest = int(node)
+    
+    return(int(largest) + 1)
     
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 def testDisplay(G, isFinished):
@@ -66,16 +85,61 @@ def testDisplay(G, isFinished):
     plt.show() #remove in final
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-def equationCheck(G, H):
-    #this function is responsible for determining if the equation holds true
+def scaleGraph(G):
+    #Iterate through the nodes of the graph and scale each edge weight to (n/2)*(1/sum)
 
-    #if()
+    Gprime = nx.minimum_spanning_tree(G, weight='weight', algorithm='kruskal', ignore_nan=False) #takes the mst of the spanned G'
 
-    
+    sum = Gprime.size(weight="weight")
+    n = Gprime.number_of_nodes()
+     
 
-    print("incomplete")
+    for u,v,d in Gprime.edges(data=True): #scaling
+        d['weight'] *= round(((n/2) * (1/sum)), 2)
 
-    return(True) #use this to set isFinished to True so the test display can change start.png to end.png for the html visualization
+    return(Gprime)
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def subdivideGraph(Gprime, H):
+    #iterate through all the edges 
+    # if > 1: split each edge into mini edges and then fill those gaps with nodes
+    #then add those mini edges and nodes to H
+    #if <= 1, add anyway
+
+    for u,v,d in Gprime.edges(data=True): #subdivision w(e) / ceiling(w(e))
+        if(d['weight'] > 1):
+            newEdgeCount = round((d['weight']) / (math.ceil(d['weight'])))
+            print(newEdgeCount)
+            newEdgeWeight = (d['weight']) / (newEdgeCount)
+            print(newEdgeWeight)
+            lastNode = u
+
+            for i in range(newEdgeCount):
+                tempNode = nodeCreation(H)
+                H.add_node(tempNode, weight=newEdgeWeight)
+                H.add_edge(lastNode, tempNode)
+                lastNode = tempNode
+                print("adding")
+
+            H.add_edge(lastNode, v) #completes the chain
+
+        else:
+            H.add_edge(u,v, weight = d['weight'])
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def dLightWeight():
+    #add mst to H, then add edges from g' until the sum <= d
+
+    print('incomplete')
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def spannerCompletion():
+    #take each pair of vertex, calc the shortest path from u,v in H and u,v in G' check how much larger is H
+    #if it is a lot larger and doesn't satisfy the equation, add all the edges into H from G' (for u,v)
+    #do until this has been done for all vertex pairs
+    #if there is a pair that is not in H but in G', then just add from G' to H
+
+    print('incomplete')
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 def spanning(G, H):
@@ -93,30 +157,7 @@ def spanning(G, H):
     """
 
     mst = nx.minimum_spanning_tree(G) #get mst of G
-    sum = mst.size(weight="weight")
-    n = mst.number_of_nodes()
-
-    for u,v,d in G.edges(data=True): #scaling
-        d['weight'] *= (n/2)*(1/sum)
-
-
-    for u,v,d in G.edges(data=True): #subdivision w(e) / ceiling(w(e))
-        #iterate through all the edges, if > 1: split each edge into mini edges and then fill those gaps with nodes
-        #then add those mini edges and nodes to H
-        #if <= 1, add anyway
-
-        print()
-
-    #d- light weight
-    #add mst to H, then add edges from g' until the sum <= d
-
-    #spanner completion
-    #take each pair of vertex, calc the shortest path from u,v in H and u,v in G' check how much larger is H
-    #if it is a lot larger and doesn't satisfy the equation, add all the edges into H from G' (for u,v)
-    #do until this has been done for all vertex pairs
-    #if there is a pair that is not in H but in G', then just add from G' to H
-
-
+    
     return(H)
 
 
